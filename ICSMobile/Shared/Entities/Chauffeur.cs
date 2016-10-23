e51a20887f0a916df678df096bcb9531c892862f
dimitri <dimitrivantillo@gmail.com>
2016-10-23 21:51:36 +0200

    Oude domain eruit gezwierd (was iets met portable) en shared toegevoegd voor de entities

---
+++ ICSMobile/Shared/Entities/Chauffeur.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Domain.Entities
+{
+    class Chauffeur : IdentityUser
+    {
+        public override string Id { get; set; }
+        public override string UserName { get; set; }
+        public override string Email { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
