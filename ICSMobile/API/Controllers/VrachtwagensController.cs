6577f13366d18721af5fc0994f7ff7fb92be2491
dimyke <dimitrivantillo@gmail.com>
2016-11-16 15:22:19 +0100

    iets geprobeerd met vrachtwagens api but failed.

--- ICSMobile/API/Controllers/VrachtwagensController.cs
+++ ICSMobile/API/Controllers/VrachtwagensController.cs
@@ -10,24 +10,28 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using API.Models;
 using Shared.Entities;
+using BL.Managers;
 
 namespace API.Controllers
 {
+    [RoutePrefix("api/vrachtwagens")]
+    [AllowAnonymous]
     public class VrachtwagensController : ApiController
     {
-        private APIContext db = new APIContext();
+        private readonly VrachtwagenManager _vrachtwagenManager = new VrachtwagenManager();
 
         // GET: api/Vrachtwagens
-        public IQueryable<Vrachtwagen> GetVrachtwagens()
-        {
-            return db.Vrachtwagens;
-        }
+        //public IQueryable<Vrachtwagen> GetVrachtwagens()
+        //{
+        //    return Ok(_vrachtwagenManager.AlleVrachtwagenen());
+        //}
 
         // GET: api/Vrachtwagens/5
+        [Route("byId")]
         [ResponseType(typeof(Vrachtwagen))]
         public IHttpActionResult GetVrachtwagen(string id)
         {
-            Vrachtwagen vrachtwagen = db.Vrachtwagens.Find(id);
+            Vrachtwagen vrachtwagen = _vrachtwagenManager.FindVrachtwagen(id);
             if (vrachtwagen == null)
             {
                 return NotFound();
@@ -35,100 +39,102 @@ namespace API.Controllers
 
             return Ok(vrachtwagen);
         }
+
+
 
         // PUT: api/Vrachtwagens/5
-        [ResponseType(typeof(void))]
-        public IHttpActionResult PutVrachtwagen(string id, Vrachtwagen vrachtwagen)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //        [ResponseType(typeof(void))]
+        //        public IHttpActionResult PutVrachtwagen(string id, Vrachtwagen vrachtwagen)
+        //        {
+        //            if (!ModelState.IsValid)
+        //            {
+        //                return BadRequest(ModelState);
+        //            }
 
-            if (id != vrachtwagen.NummerPlaat)
-            {
-                return BadRequest();
-            }
+        //            if (id != vrachtwagen.NummerPlaat)
+        //            {
+        //                return BadRequest();
+        //            }
 
-            db.Entry(vrachtwagen).State = EntityState.Modified;
+        //            db.Entry(vrachtwagen).State = EntityState.Modified;
 
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!VrachtwagenExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //            try
+        //            {
+        //                db.SaveChanges();
+        //            }
+        //            catch (DbUpdateConcurrencyException)
+        //            {
+        //                if (!VrachtwagenExists(id))
+        //                {
+        //                    return NotFound();
+        //                }
+        //                else
+        //                {
+        //                    throw;
+        //                }
+        //            }
 
-            return StatusCode(HttpStatusCode.NoContent);
-        }
+        //            return StatusCode(HttpStatusCode.NoContent);
+        //        }
 
-        // POST: api/Vrachtwagens
-        [ResponseType(typeof(Vrachtwagen))]
-        public IHttpActionResult PostVrachtwagen(Vrachtwagen vrachtwagen)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //        // POST: api/Vrachtwagens
+        //        [ResponseType(typeof(Vrachtwagen))]
+        //        public IHttpActionResult PostVrachtwagen(Vrachtwagen vrachtwagen)
+        //        {
+        //            if (!ModelState.IsValid)
+        //            {
+        //                return BadRequest(ModelState);
+        //            }
 
-            db.Vrachtwagens.Add(vrachtwagen);
+        //            db.Vrachtwagens.Add(vrachtwagen);
 
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateException)
-            {
-                if (VrachtwagenExists(vrachtwagen.NummerPlaat))
-                {
-                    return Conflict();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //            try
+        //            {
+        //                db.SaveChanges();
+        //            }
+        //            catch (DbUpdateException)
+        //            {
+        //                if (VrachtwagenExists(vrachtwagen.NummerPlaat))
+        //                {
+        //                    return Conflict();
+        //                }
+        //                else
+        //                {
+        //                    throw;
+        //                }
+        //            }
 
-            return CreatedAtRoute("DefaultApi", new { id = vrachtwagen.NummerPlaat }, vrachtwagen);
-        }
+        //            return CreatedAtRoute("DefaultApi", new { id = vrachtwagen.NummerPlaat }, vrachtwagen);
+        //        }
 
-        // DELETE: api/Vrachtwagens/5
-        [ResponseType(typeof(Vrachtwagen))]
-        public IHttpActionResult DeleteVrachtwagen(string id)
-        {
-            Vrachtwagen vrachtwagen = db.Vrachtwagens.Find(id);
-            if (vrachtwagen == null)
-            {
-                return NotFound();
-            }
+        //        // DELETE: api/Vrachtwagens/5
+        //        [ResponseType(typeof(Vrachtwagen))]
+        //        public IHttpActionResult DeleteVrachtwagen(string id)
+        //        {
+        //            Vrachtwagen vrachtwagen = db.Vrachtwagens.Find(id);
+        //            if (vrachtwagen == null)
+        //            {
+        //                return NotFound();
+        //            }
 
-            db.Vrachtwagens.Remove(vrachtwagen);
-            db.SaveChanges();
+        //            db.Vrachtwagens.Remove(vrachtwagen);
+        //            db.SaveChanges();
 
-            return Ok(vrachtwagen);
-        }
+        //            return Ok(vrachtwagen);
+        //        }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        //        protected override void Dispose(bool disposing)
+        //        {
+        //            if (disposing)
+        //            {
+        //                db.Dispose();
+        //            }
+        //            base.Dispose(disposing);
+        //        }
 
-        private bool VrachtwagenExists(string id)
-        {
-            return db.Vrachtwagens.Count(e => e.NummerPlaat == id) > 0;
-        }
+        //        private bool VrachtwagenExists(string id)
+        //        {
+        //            return db.Vrachtwagens.Count(e => e.NummerPlaat == id) > 0;
+        //        }
     }
 }
--- ICSMobile/API/Global.asax.cs
+++ ICSMobile/API/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace API
